b811f5af6798d2131775f587c58b934bb75a7d85
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2022-06-21 14:16:15 -0400

    backdoor to scan

--- ZXingNetMobTest/App.xaml.cs
+++ ZXingNetMobTest/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace ZXingNetMobTest
 {
@@ -10,7 +12,9 @@ namespace ZXingNetMobTest
     {
       InitializeComponent();
 
-      MainPage = new MainPage();
+			//MainPage = new MainPage();
+			//MainPage = new NavigationPage(new HomePage { Title = "ZXing.Net.Mobile" });
+			MainPage = new NavigationPage(new MainPage());
     }
 
     protected override void OnStart()
@@ -24,5 +28,34 @@ namespace ZXingNetMobTest
     protected override void OnResume()
     {
     }
-  }
+
+		public void UITestBackdoorScan(string param)
+		{
+			var expectedFormat = ZXing.BarcodeFormat.QR_CODE;
+			Enum.TryParse(param, out expectedFormat);
+			var opts = new ZXing.Mobile.MobileBarcodeScanningOptions
+			{
+				PossibleFormats = new List<ZXing.BarcodeFormat> { expectedFormat }
+			};
+
+			System.Diagnostics.Debug.WriteLine("Scanning " + expectedFormat);
+
+			var scanPage = new ZXingScannerPage(opts);
+			scanPage.OnScanResult += (result) =>
+			{
+				scanPage.IsScanning = false;
+
+				Device.BeginInvokeOnMainThread(() =>
+				{
+					var format = result?.BarcodeFormat.ToString() ?? string.Empty;
+					var value = result?.Text ?? string.Empty;
+
+					MainPage.Navigation.PopAsync();
+					MainPage.DisplayAlert("Barcode Result", format + "|" + value, "OK");
+				});
+			};
+
+			MainPage.Navigation.PushAsync(scanPage);
+		}
+	}
 }
