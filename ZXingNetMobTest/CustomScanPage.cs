9837f6fdbefff0dcfff3cc2bc020b67ad918cf68
Artemio Banos Gallardo <Artemio_Banos_gallar@Dellteam.com>
2022-06-24 13:57:46 -0400

    added constructor and new method to select res

--- ZXingNetMobTest/CustomScanPage.cs
+++ ZXingNetMobTest/CustomScanPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -145,6 +146,71 @@ namespace ZXingNetMobTest
       this.BindingContext = this;
     }
 
+    public CustomScanPage(List<CameraResolution> cameraResolutions) : base()
+    {
+      this.AvailableResolutions = cameraResolutions;
+
+      zxing = new ZXingScannerView
+      {
+        HorizontalOptions = LayoutOptions.FillAndExpand,
+        VerticalOptions = LayoutOptions.FillAndExpand,
+        AutomationId = "zxingScannerView",
+      };
+      zxing.OnScanResult += (result) =>
+        Device.BeginInvokeOnMainThread(async () =>
+        {
+          // Stop analysis until we navigate away so we don't keep reading barcodes
+          zxing.IsAnalyzing = false;
+
+          // Show an alert
+          await DisplayAlert("Scanned Barcode", result.Text, "OK");
+
+          // Navigate away
+          await Navigation.PopAsync();
+        });
+
+      overlay = new ZXingDefaultOverlay
+      {
+        TopText = "Hold your phone up to the barcode",
+        BottomText = "Scanning will happen automatically \n For Barcode 128 ~640x400 res is advised",
+        ShowFlashButton = zxing.HasTorch,
+        AutomationId = "zxingDefaultOverlay",
+      };
+      overlay.FlashButtonClicked += (sender, e) =>
+      {
+        zxing.IsTorchOn = !zxing.IsTorchOn;
+      };
+      var grid = new Grid
+      {
+        VerticalOptions = LayoutOptions.FillAndExpand,
+        HorizontalOptions = LayoutOptions.FillAndExpand,
+      };
+
+      zxing.Options.CameraResolutionSelector = this.SelectResolutionMatchingDisplayAspectRatio;
+
+      var picker = new Picker()
+      {
+        Title = "Select a resolution",
+        ItemsSource = this.AvailableResolutionsString,
+        WidthRequest = 100,
+        HeightRequest = 50,
+        HorizontalOptions = LayoutOptions.Center,
+        VerticalOptions = LayoutOptions.End
+      };
+
+      picker.PropertyChanged += this.APropertyChanged;
+      picker.SetBinding(Picker.ItemsSourceProperty, nameof(this.AvailableResolutionsString));
+      picker.SetBinding(Picker.SelectedIndexProperty, nameof(this.SelectedResolutionIndex));
+
+      grid.Children.Add(zxing);
+      grid.Children.Add(overlay);
+      grid.Children.Add(picker);
+
+      // The root page of your application
+      Content = grid;
+      this.BindingContext = this;
+    }
+
     private CameraResolution SetAvailableResolutions(List<CameraResolution> availableResolutions)
     {
       if (this.AvailableResolutions == null)
@@ -162,6 +228,36 @@ namespace ZXingNetMobTest
       this.RefreshBindings();
 
       return this.SelectedResolution;
+    }
+
+    private CameraResolution SelectResolutionMatchingDisplayAspectRatio(List<CameraResolution> availableResolutions)
+    {
+      // Setting a resolution not in the availables will cause an exception
+      // Todo: [EHS] Find out where to handle this exception
+      if (availableResolutions == null || availableResolutions.Count < 1)
+      {
+        throw new ArgumentNullException(nameof(availableResolutions));
+      }
+
+      // There is no reading issues on iOS, returning highest resolution
+      if (DeviceInfo.Platform == DevicePlatform.iOS)
+      {
+        return this.SelectLowestResolutionMatchingDisplayAspectRatio(availableResolutions);
+      }
+
+      // Capturing input data in native resolution may create massive data which will slow the barcode reading. Android recommends around 2 Megapixels.
+      // Selecting a resolution < 2 Mega Pixels for better performance. The smaller the fastest but accuracy get compromized.
+      // TODO: [EHS] Very slow for Code38 and 128. At ~640*500 it is much faster but may affect some users.
+      int seed = Convert.ToInt32(1280 * 720);
+
+      var resolutionsLessThan2MegasPixels = availableResolutions.Where(r => r.Width * r.Height <= seed).ToList();
+
+      var resWithAspectRatio = this.SelectLowestResolutionMatchingDisplayAspectRatio(resolutionsLessThan2MegasPixels);
+
+      //var pixels = availableResolutions.Select(r => r.Width * r.Height).ToArray();
+      //var performanceResIndex = this.FindIndexOfCloserToNumber(seed: seed, pixels);
+      //return availableResolutions[performanceResIndex];
+      return resWithAspectRatio;
     }
 
     public CameraResolution SelectLowestResolutionMatchingDisplayAspectRatio(List<CameraResolution> availableResolutions)
@@ -210,6 +306,17 @@ namespace ZXingNetMobTest
       //this.OnPropertyChanged(nameof(this.AvailableResolutions));
       this.OnPropertyChanged(nameof(this.SelectedResolution));
       this.OnPropertyChanged(nameof(this.SelectedResolutionIndex));
+    }
+
+
+
+    private void APropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      var picker = (Picker)sender;
+
+      var index = picker.SelectedIndex;
+      var elements = picker.Items;
+      var a = 0;
     }
   }
 }
